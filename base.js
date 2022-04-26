9832d4550a4bfe922eca8225297ade363f62437a
zarooma <97543999+zarooma@users.noreply.github.com>
2022-04-26 07:27:07 -0400

    Update base.js

--- base.js
+++ base.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
+var kn = {hp:150,dam:20,speed:15,mag:3,defence:12,wepon1:startsword,wepon2:"none",items:items,}; 
+var user = console.prompt
 
-
-
-fs.writeFile('playerdate.txt'," players name: " + user + " pasword: " + pas + " class: " + pcl + " items " + items + , function (err) {
+fs.appendFile('playerdate.txt'," players name: " + user + " pasword: " + pas + " class: " + pcl + " items " + items + , function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
