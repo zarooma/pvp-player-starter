11381443f2b07737c4c8181383bc51f5d15f7532
zarooma <97543999+zarooma@users.noreply.github.com>
2022-04-26 06:45:44 -0400

    Update base.js

--- base.js
+++ base.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
-fs.writeFile('playerdate.txt', 'Hello content!', function (err) {
+
+
+
+fs.writeFile('playerdate.txt'," players name: " + user + " pasword: " + pas + " class: " + pcl + " items " + items + , function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
