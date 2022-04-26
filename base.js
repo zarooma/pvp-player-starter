b48482f8ecefc0de5f5c29bca36215c8583f52a2
zarooma <97543999+zarooma@users.noreply.github.com>
2022-04-26 07:48:40 -0400

    Update base.js
    fs.appendFile( user +'.txt'," players name: " + user + " pasword: " + pas + " class: " + pcl + " items " + items + , function (err) {
      if (err) throw err;
      console.log('Saved!');
    });

--- base.js
+++ base.js
@@ -1,8 +1,27 @@
 var fs = require('fs');
+var 
 var kn = {hp:150,dam:20,speed:15,mag:3,defence:12,wepon1:startsword,wepon2:"none",items:items,}; 
-var user = console.prompt
-
-fs.appendFile('playerdate.txt'," players name: " + user + " pasword: " + pas + " class: " + pcl + " items " + items + , function (err) {
+var user = prompt("username: ")
+var pas = prompt("pasword: ")
+var items = "noune"
+var login = prompt("chose one: ")
+var pickcl = "knight mage archer assian thief chose one"
+var pcl = prompt("")
+console.log("if you have a acount type login if you dont type reg ")
+login
+if (login == "reg") {
+console.log("tpye username);
+  user
+console.log("type pasword");
+  pas
+console.log("chouse a class")
+console.log(pickcl)
+  pcl
+  
+  fs.appendFile( user +'.txt'," players name: " + user + " pasword: " + pas + " class: " + pcl +, function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
+  
+}
+
